8f6e78be84e0bf45ec6717d9180b94172df85dfd
Adam Sayer <asayer82@icloud.com>
2024-08-12 21:42:05 +1000

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Adam Sayer on 12/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
